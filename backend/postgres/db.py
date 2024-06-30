81535be6fa8d77b7ae62b5f02c2b5971b5336e5c
rishikeshydv <rishikeshadh4@gmail.com>
2024-06-30 01:45:12 -0400

    feat: database integration added

---
+++ backend/postgres/db.py
@@ -0,0 +1,13 @@
+import psycopg2
+
+conn = psycopg2.connect(
+    host="localhost",
+    database="postgres",
+    user=" ",
+    password=" ",
+    port="5432"
+)
+
+cursor = conn.cursor()
+cursor.execute("SELECT * FROM users")
+print(cursor.fetchall())
