7948265bb3f10874654388911ba6fec428781bc9
rishikeshydv <rishikeshadh4@gmail.com>
2024-07-01 02:05:47 -0400

    feat: database imports done

---
+++ backend/firebase/db.py
@@ -0,0 +1,22 @@
+import firebase_admin
+from firebase_admin import credentials
+from firebase_admin import firestore
+
+class FirebaseConfig:
+    def __init__(self):
+        if not firebase_admin._apps:
+            cred = credentials.Certificate("backend/firebase/config.json")
+            firebase_admin.initialize_app(cred, {
+                'databaseURL': 'https://speety-2175-default-rtdb.firebaseio.com'
+            })
+        self.db = firestore.client()
+    
+    def initialize_firebase(self):
+        return self.db
+        #show the information in the document
+        # info = db.collection('User_Info').document('testuser1@gmail.com').get().to_dict()
+        # print(info)
+
+# Export the db instance
+firebase_config = FirebaseConfig()
+db = firebase_config.initialize_firebase()
--- backend/src/price-trend/main.py
+++ backend/src/price-trend/main.py
@@ -4,14 +4,19 @@
 #create a new collection called property_history
 #property_history -> property_id -> all information
 #make sure the property IDs are same for 'presentListings' and 'propertyHistory'
-from main import FirebaseConfig
-
+import os
+import sys
+sys.path.append(os.path.join(os.path.dirname(__file__)))
+from backend.firebase import FirebaseConfig
 class PriceTrend():
     def __init__(self):
-        self.db = FirebaseConfig().initialize_firebase()
+        self.database = FirebaseConfig().initialize_firebase()
 
     #returns the list [year, price] for the given property_id after retrieving the data from the database
     def individual_property_price_trend(self, property_id):
-        propertyData = self.db.collection('property_history').document(property_id).get().to_dict()
+        propertyData = self.database.collection('property_history').document(property_id).get().to_dict()
         return propertyData['price']
+    
+if __name__ == "__main__":
+    print(PriceTrend().individual_property_price_trend('uuidv4'))
 
