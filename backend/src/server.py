339dc3f8833369f4d6cd03cae6fd55044b5d4eb0
rishikeshydv <rishikeshadh4@gmail.com>
2024-07-02 16:53:09 -0400

    feat: price-estimator-model init

--- backend/src/server.py
+++ backend/src/server.py
@@ -2,6 +2,7 @@ from flask import Flask, jsonify, request
 from waitress import serve
 from flask_cors import CORS
 import logging
+import requests
 
 # Setting up a Flask app
 app = Flask(__name__)
@@ -13,6 +14,7 @@ logging.basicConfig(level=logging.DEBUG)
 # Setting up a GPT model API key (placeholder)
 apiKey = "your_api_key"
 
+#price trent for individual property
 @app.route('/api/v1/price-trend/individual-property/<property_id>', methods=['GET'])
 def individual_property_price_trend(property_id):
     try:
@@ -20,6 +22,52 @@ def individual_property_price_trend(property_id):
         # For now, we return a placeholder message
         price_trend = f"Single Property Price {property_id}"
         return jsonify(message=price_trend), 200
+    except Exception as e:
+        # Log the error message
+        app.logger.error(f"Error: {e}")
+        return jsonify(error="Internal Server Error"), 500
+    
+#getting geospatial information of the given latitude and longitude
+@app.route('/api/v1/geospatial/<lat>/<lng>/<type>', methods=['GET'])
+def geospatial(lat,lng, type):
+    try:
+        url = 'https://places.googleapis.com/v1/places:searchNearby'
+        data = {
+    "includedTypes": ["liquor_store", "convenience_store"],
+    "maxResultCount": 10,
+    "locationRestriction": {
+        "circle": {
+            "center": {
+                "latitude": 37.7937,
+                "longitude": -122.3965
+            },
+            "radius": 1000.0
+        }
+    }
+}
+        headers = {
+    'Content-Type': 'application/json',
+    'X-Goog-Api-Key': 'AIzaSyAvamq-1AR2paooKX-Hq7LvyyfIbwNsVVU',
+    'X-Goog-FieldMask': 'places.displayName,places.primaryType,places.types'
+}
+        geoReq = requests.get(url, headers=headers, json=data)
+        if geoReq.status_code == 200:
+            return jsonify(geoReq.json()), 200
+
+    except Exception as e:
+        # Log the error message
+        app.logger.error(f"Error: {e}")
+        return jsonify(error="Internal Server Error"), 500
+    
+#getting the walkability score of the given latitude and longitude
+@app.route('/api/v1/walkability/<lat>/<lng>/<fullAddress>', methods=['GET'])
+def walkability(lat,lng, fullAddress):
+    try:
+        api_key="5e775c237088586a07f1a8ba73969b1a"
+        url = f"https://api.walkscore.com/score?format=json&address={fullAddress}&lat={lat}&lon={lng}&wsapikey={api_key}"
+        response = requests.get(url)
+        return response.json()
+
     except Exception as e:
         # Log the error message
         app.logger.error(f"Error: {e}")
