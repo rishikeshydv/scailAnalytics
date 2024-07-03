49bc8a68fe0f86eca71a2da2c316328470404400
rishikeshydv <rishikeshadh4@gmail.com>
2024-07-03 10:00:55 -0400

    feat: CNN api connection

--- backend/src/server.py
+++ backend/src/server.py
@@ -3,7 +3,12 @@ from waitress import serve
 from flask_cors import CORS
 import logging
 import requests
-
+from keras.models import load_model
+from sklearn.preprocessing import StandardScaler,OneHotEncoder
+import pandas as pd
+import numpy as np
+from sklearn.compose import ColumnTransformer
+from sklearn.pipeline import Pipeline
 # Setting up a Flask app
 app = Flask(__name__)
 CORS(app)  # Enable CORS
@@ -13,6 +18,16 @@ logging.basicConfig(level=logging.DEBUG)
 
 # Setting up a GPT model API key (placeholder)
 apiKey = "your_api_key"
+
+#loading the model
+model = load_model('backend/src/house_price_prediction_model.h5')
+
+
+numerical_features = ['bed', 'bath', 'acre_lot', 'house_size','zip_code','street']
+categorical_features = ['city', 'state']
+
+numerical_transformer = StandardScaler()
+categorical_transformer = OneHotEncoder()
 
 #price trent for individual property
 @app.route('/api/v1/price-trend/individual-property/<property_id>', methods=['GET'])
@@ -67,6 +82,23 @@ def walkability(lat,lng, fullAddress):
         url = f"https://api.walkscore.com/score?format=json&address={fullAddress}&lat={lat}&lon={lng}&wsapikey={api_key}"
         response = requests.get(url)
         return response.json()
+
+    except Exception as e:
+        # Log the error message
+        app.logger.error(f"Error: {e}")
+        return jsonify(error="Internal Server Error"), 500
+    
+#predicting the price of the house
+@app.route('/api/v1/predict-price', methods=['POST'])
+def predict_price():
+    try:
+        df=pd.DataFrame(request.json)
+        numerical_data = numerical_transformer.transform(df[numerical_features])
+        categorical_data = categorical_transformer.transform(df[categorical_features]).toarray()
+        features = np.concatenate([numerical_data, categorical_data], axis=1)
+
+        predicted_price = model.predict(features)
+        return predicted_price[0][0]
 
     except Exception as e:
         # Log the error message
